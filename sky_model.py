8f1cd1b0020c61a96bcbeaac879f06fc8a361b6d
MaudeCharmetant <58000492+MaudeCharmetant@users.noreply.github.com>
2020-01-23 17:11:28 +0100

    Update sky_model.py

--- sky_model.py
+++ sky_model.py
@@ -1,1 +1,103 @@
+import numpy as np 
+import healpy as hp
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
 
+def pix_reso(nside,arcmin=True): 
+    
+            
+    """
+    Compute the resolution of Healpy pixels in function of the nside of the map.  
+
+    Parameters
+    ----------
+    
+    nside : int
+        Nside, number of separation of the healpy blocks. 
+    arcmin : bool
+        if True return the resolution in arcmin, otherwise in radians. 
+        
+    Returns
+    -------
+    float
+        The resolution of one healpy pixel. 
+
+    """
+    
+    #Number of pixels : 
+    N_pix = hp.pixelfunc.nside2npix(nside)
+    
+    #Compute the unit : 
+    if arcmin == True :        
+        multi=(180*60/np.pi)        
+    else:        
+        multi=1
+        
+    #Compute the resolution :     
+    reso = np.sqrt(((4*np.pi)/N_pix)*multi**2)
+    
+    #Feedback user : 
+    print('The pixel resolution has been computed')
+
+    return reso
+  
+  
+  def WN_map(nside,data_path,file_out,noise,unit_noise,arcmin,units,pictures_path): 
+
+        
+    """
+    Function which create a White noise map for a given noise/arcmin or noise/radians.  
+
+    Parameters
+    ----------
+    
+    nside : int
+        Nside, number of separation of the healpy blocks. 
+    data_path : str
+        Path where the datas will be stored.
+    file_out : str
+        Name of the White noise healpy map.
+    noise : float 
+        noise value desired in any units by radians or arcmin.
+    unit_noise : float 
+        resolution of the noise, for exemple 1' or 1 radians. 
+    arcmin : bool 
+        if true mean that the noise is given in /arcmin. 
+    units : str 
+        units of the noise 
+    pictures_path : str 
+        path where the pictures will be stored.
+        
+    Returns
+    -------
+    array
+        Array contaning the Variarion of intensity produced by tSZ over the fequencies. 
+
+    """
+    
+    #Compute the average noise in each pixel : 
+    noise = (noise * unit_noise)/pix_reso(nside,arcmin)
+        
+    #Create White-noise map :
+    Npix = hp.pixelfunc.nside2npix(nside) #Compute the number of pixels
+    m =  np.random.normal(0,noise,Npix) #Random normal distribution centered over the desired noise
+    
+    #Display and save the distribution: 
+    plt.hist(m,200,normed=True) #Display histogram of the distribution
+    plt.title('$Noise$ $distribution$')
+    plt.xlabel('$Noise$ $in$ '+ units)
+    plt.ylabel('$Distribution$')
+    plt.savefig(pictures_path + 'WN_PDF_'+str(int(noise))+units+'.png')
+
+    #Display map and save map: 
+    WN_map = hp.write_map(data_path + file_out,m, overwrite=True) #Save the distribution into a healpy map
+    hp.mollview(m, title="White Noise map : n= "+ str(int(noise)) + units, norm='hist',unit=units)
+    plt.savefig(pictures_path + 'WN_map_'+str(int(noise))+units+'.png') 
+    plt.show()
+    
+    return WN_map 
+  
+  
+  
